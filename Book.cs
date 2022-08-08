dfc32c3c9f46cac0d3ec57ce19c4fbbe7424458f
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 16:33:22 +1200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningConstructors
+{
+
+    //this is a class that has been assigned properties - these properties can have values assigned to them.
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+
+        //whenever we mention something like public followed by the name of the class this is what is known as a constructor... so now we dont have to assignn values to the properties of the object manually every single time. The program will now know to accept the values and apply them to the object without having to specify which property the value is for.
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //this creates a new instance of the class - this is called an object and can now be used similar to a variable
+            Book book1 = new Book("Harry Potter", "J.K. Rowling", 400);
+
+
+            // we are able to assign values this way without declaring the property of the object because we have made a constructor for this object's class. 
+            Book book2 = new Book("Lord of the Rings", "J.R.R. Tolkien", 650);
+
+
+            Console.WriteLine(book1.title);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
